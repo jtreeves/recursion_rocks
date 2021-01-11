7a5a5683a3e40eee819dd13cf44fcd9acc46ad3c
jtreeves <jackson.t.reeves@gmail.com>
2021-01-10 23:39:31 -0500

    [find_max] Add assert statements to tests

--- find_max.py
+++ find_max.py
@@ -11,9 +11,17 @@ def find_max(l):
 
 print(find_max([1, 4, 45, 6, -50, 10, 2]))
 # => 45
+assert(find_max([1, 4, 45, 6, -50, 10, 2]) == 45)
+# => True
 print(find_max([198, -487, 68, -5, 321, 11, 226]))
 # => 321
+assert(find_max([198, -487, 68, -5, 321, 11, 226]) == 321)
+# => True
 print(find_max([2, 3, 5, 7]))
 # => 7
+assert(find_max([2, 3, 5, 7]) == 7)
+# => True
 print(find_max([10000, 5000, 2500, 1250]))
 # => 10000
+assert(find_max([10000, 5000, 2500, 1250]) == 10000)
+# => True
