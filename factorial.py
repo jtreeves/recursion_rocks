edf647504ffae456ca50fd68478a242d183eedb4
jtreeves <jackson.t.reeves@gmail.com>
2021-01-10 20:37:27 -0500

    [factorial] Update function with returns instead of prints

--- factorial.py
+++ factorial.py
@@ -8,14 +8,21 @@ def factorial(n):
         raise TypeError('Input needs to be a whole number')
     elif n < 0:
         raise ValueError('Number needs to be a whole number')
-    elif n == 0:
-        print(1)
+    if n == 0:
+        return 1
     elif n <= 2:
-        print(n)
+        return n
     else:
         product = 1
         for i in range(1, n + 1):
             product *= i
         return product
 
+print(factorial(0))
+# => 1
+print(factorial(3))
+# => 6
 print(factorial(5))
+# => 120
+print(factorial(7))
+# => 5040
