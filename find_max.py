5a2808bf8e58bb43eda404625e16b33091efd239
jtreeves <jackson.t.reeves@gmail.com>
2021-01-10 21:07:51 -0500

    [find_max] Add test cases

--- find_max.py
+++ find_max.py
@@ -7,5 +7,11 @@ def find_max(l):
     # Write code here
     pass
 
-# print(find_max([1, 4, 45, 6, -50, 10, 2]))
+print(find_max([1, 4, 45, 6, -50, 10, 2]))
 # => 45
+print(find_max([198, -487, 321, 68, -5, 11, 226]))
+# => 321
+print(find_max([2, 3, 5, 7, 11]))
+# => 11
+print(find_max([10000, 5000, 2500, 1250, 625]))
+# => 10000
